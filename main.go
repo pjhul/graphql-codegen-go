197d0de799a9c3fa734d43ec391f3fcc5361b503
Paul Hultgren <pjhultgren5@gmail.com>
2021-08-21 15:32:01 -0400

    panic on failure to parse query documents

--- main.go
+++ main.go
@@ -77,6 +77,7 @@ func main() {
 		}
 
 		queryDoc, err := parseQueryDocuments(schema, queryDocs)
+		if err != nil { panic(err) }
 
 		err = generateOperations(schema, queryDoc, &buf)
 		if err != nil { panic(err) }
@@ -89,29 +90,4 @@ func main() {
 		if err != nil { panic(err) }
 
 		fmt.Println("Successfully generated schema file!")
-
-		/*fmt.Printf("Reading schema from %s\n", *schemaPath)
-		content, err := ioutil.ReadFile(*schemaPath)
-		if err != nil { panic(err) }
-
-		source := &ast.Source{
-				Name: *schemaPath,
-				Input: string(content),
-				BuiltIn: false,
-		}
-
-		schemaDoc, gqlErr := gqlparser.LoadSchema(source)
-		if gqlErr != nil { panic(gqlErr) }
-
-		err = generateSchema(schemaDoc, &buf)
-		if err != nil { panic(err) }
-
-		file, err := os.Create("schema.go")
-		if err != nil { panic(err) }
-		defer file.Close()
-
-		err = gofmt.ProcessFile("./schema.go", &buf, file, false)
-		if err != nil { panic(err) }
-
-		*/
 }
